3c322d0285e80a4bb5f5c431f20f4a530ca6bb3c
Radosław Gierwiało <github@radziel.com>
2024-11-09 17:58:19 +0100

    New version

--- services/backend/src/app.py
+++ services/backend/src/app.py
@@ -1,109 +1,44 @@
-import os
-import sqlite3
-import time
-import asyncio
-from shazamio import Shazam
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, g
 from flask_cors import CORS
+
+from models.db import Database
+from models.shazam import ShazamWrapper
 
 app = Flask(__name__)
 
 SAMPLE_STORAGE = "/tmp/samples"
 SAMPLE_DB = "/tmp/samples/music_library.db"
-os.makedirs(SAMPLE_STORAGE, exist_ok=True)
 
 CORS(app)
 
-def initialize_database():
-    connection = sqlite3.connect(SAMPLE_DB)
-    cursor = connection.cursor()
+app.config['DATABASE'] = Database(SAMPLE_DB, SAMPLE_STORAGE)
+app.config['SHAZAM'] = ShazamWrapper()
 
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS songs (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        timestamp TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
-        title TEXT NOT NULL,
-        artist TEXT NOT NULL
-    )
-    """)
+@app.before_request
+def load_resources():
+    g.db = app.config['DATABASE']
+    g.shazam = app.config['SHAZAM']
 
-    cursor.execute("CREATE INDEX IF NOT EXISTS idx_timestamp ON songs (timestamp)")
-
-    connection.commit()
-    connection.close()
-
-def get_last_two_songs():
-    connection = sqlite3.connect(SAMPLE_DB)
-    cursor = connection.cursor()
-
-    cursor.execute("""
-    SELECT title, artist, timestamp
-    FROM songs
-    ORDER BY timestamp DESC
-    LIMIT 2
-    """)
-
-    rows = cursor.fetchall()
-
-    result = {
-        "0": {
-            "title": rows[0][0] if len(rows) > 0 else None,
-            "artist": rows[0][1] if len(rows) > 0 else None
-        },
-        "1": {
-            "title": rows[1][0] if len(rows) > 1 else None,
-            "artist": rows[1][1] if len(rows) > 1 else None
-        }
-    }
-
-    connection.close()
-    return result
-
-initialize_database()
-
-async def recognize(file_path):
-    return await Shazam().recognize(file_path)
-
-@app.route('/detect', methods=['POST'])
+@app.route('/detect', methods=['GET','POST'])
 def detect():
-    if 'audio' not in request.files:
+    file_path = g.db.saveAudioSample(request)
+
+    if file_path is None:
         return jsonify({}), 400
 
-    audio_file = request.files['audio']
-
-    if audio_file.filename == '':
-        return jsonify({}), 400
-
-    audio_data = audio_file.read()
-
-    timestamp = int(time.time())
-
-    file_path = os.path.join(SAMPLE_STORAGE, f"{timestamp}.webm")
-
-    with open(file_path, 'wb') as f:
-        f.write(audio_data)
-
-    out = asyncio.run(recognize(file_path))
+    out = g.shazam.recognize_song(file_path)
 
     if 'track' not in out:
         return jsonify({}), 400
     
     out = out['track']
     
-    connection = sqlite3.connect(SAMPLE_DB)
-    cursor = connection.cursor()
-    
-    cursor.execute("SELECT title, artist FROM songs ORDER BY timestamp DESC LIMIT 1")
-    last_song = cursor.fetchone()
+    last_song = g.db.getLastSong()
 
     if not last_song or (last_song[0] != out['title'] or last_song[1] != out['subtitle']):
-
-        cursor.execute("INSERT INTO songs (title, artist) VALUES (?, ?)", (out['title'], out['subtitle']))
-        connection.commit()
+        g.db.addSingleSong(out['title'], out['subtitle'])
     
-    connection.close()
-    return jsonify(get_last_two_songs()), 200
-
+    return jsonify(g.db.getLastTwoSongs()), 200
 
 @app.route('/')
 def hello():
---
+++ services/backend/src/models/db.py
@@ -0,0 +1,87 @@
+
+import sqlite3
+import os
+import time
+
+class Database:
+
+    def __init__(self, db_path, storage_path):
+        
+        db_exists = os.path.isfile(db_path)
+        storage_exists = os.path.isdir(storage_path)
+
+        self.connection = sqlite3.connect(db_path)
+        self.cursor = self.connection.cursor()
+        self.storage = storage_path
+
+        if not db_exists:
+            self.initializeDatabase()
+
+        if not storage_exists:
+            os.makedirs(storage_path, exist_ok=True)
+
+
+    def __del__(self):
+        self.connection.close()
+
+    def initializeDatabase(self):
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS songs (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            timestamp TEXT NOT NULL DEFAULT (DATETIME('now', 'localtime')),
+            title TEXT NOT NULL,
+            artist TEXT NOT NULL
+        )
+        """)
+        self.cursor.execute("CREATE INDEX IF NOT EXISTS idx_timestamp ON songs (timestamp)")
+        self.connection.commit()
+
+    def getLastSong(self):
+        query = "SELECT title, artist FROM songs ORDER BY timestamp DESC LIMIT 1"
+        self.cursor.execute(query)
+        return self.cursor.fetchone()
+
+    def getLastTwoSongs(self):
+        query = """
+        SELECT title, artist, timestamp FROM songs ORDER BY timestamp DESC
+        LIMIT 2
+        """
+        
+        self.cursor.execute(query)
+        rows = self.cursor.fetchall()
+
+        result = {
+            "0": {
+                "title": rows[0][0] if len(rows) > 0 else None,
+                "artist": rows[0][1] if len(rows) > 0 else None
+            },
+            "1": {
+                "title": rows[1][0] if len(rows) > 1 else None,
+                "artist": rows[1][1] if len(rows) > 1 else None
+            }
+        }
+
+        return result
+        
+    def addSingleSong(self, title, artist):
+        query = "INSERT INTO songs (title, artist) VALUES (?, ?)"
+        self.cursor.execute(query, (title, artist))
+        self.connection.commit()
+
+    def saveAudioSample(self, request):
+        if 'audio' not in request.files:
+            return None
+
+        audio_file = request.files['audio']
+
+        if audio_file.filename == '':
+            return None
+
+        audio_data = audio_file.read()
+
+        file_path = os.path.join(self.storage, f"{int(time.time())}.webm")
+
+        with open(file_path, 'wb') as f:
+            f.write(audio_data)
+
+        return file_path
